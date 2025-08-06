329bd5baaf6294dbfc98b00014efe2ce4cb81236
justinv1234 <justinveltri2005@gmaill.com>
2025-08-06 00:32:13 -0400

    updated project details page

--- frontend/src/pages/ProjectDetailsPage.jsx
+++ frontend/src/pages/ProjectDetailsPage.jsx
@@ -18,6 +18,33 @@ function ProjectDetail() {
   }
 
   return (
+    <div className="flex flex-col md:text-left text-center">
+      <h1 className="text-5xl font-semibold mb-10">
+        {project.title.toLowerCase()}.
+      </h1>
+      <img
+        src={project.websiteURL}
+        alt="Project screenshot"
+        className="rounded-lg border border-gray-700 mx-auto mb-5"
+      />
+      <h1 className="text-4xl font-semibold mb-3">tech stack.</h1>
+      <div className="flex flex-wrap gap-2">
+        {project.tags.map((tag, i) => (
+          <span
+            key={i}
+            className="bg-gray-600 text-white text-sm px-3 py-1 rounded-full"
+          >
+            {tag}
+          </span>
+        ))}
+      </div>
+      <h1 className="text-4xl font-semibold mt-10 mb-3">description.</h1>
+      <h1>{project.longDescription}</h1>
+    </div>
+  );
+
+  /*
+  return (
     <div className="px-8 mt-10 max-w-3xl mx-auto text-center">
       <h1 className="text-3xl font-bold mb-4">{project.title}</h1>
       <img
@@ -37,7 +64,7 @@ function ProjectDetail() {
         ))}
       </div>
     </div>
-  );
+  );*/
 }
 
 export default ProjectDetail;
